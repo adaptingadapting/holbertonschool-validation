d7104096a1093f60bea818e13b9e61e464ed1eff
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-05-14 19:07:36 -0300

    task 0

---
+++ module2_task0/README.md
@@ -0,0 +1,11 @@
+### Prerequisites
+- install hugo
+- install git
+- install makefile
+
+### Lifecycle
+- make build
+- make stop
+- make clean
+- make test
+- make help
---
+++ module2_task0/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+  "fmt"
+  "io"
+  "log"
+  "net/http"
+  "os"
+
+  "github.com/gorilla/mux"
+)
+
+func main() {
+  httpAddr := "0.0.0.0:9999"
+  if port := os.Getenv("PORT"); port != "" {
+    httpAddr = "0.0.0.0:" + port
+  }
+  fmt.Println("HTTP Server listening on", httpAddr)
+
+  // Start an HTTP server using the custom router
+  log.Fatal(http.ListenAndServe(httpAddr, setupRouter()))
+}
+
+func setupRouter() *mux.Router {
+  // Create a new empty HTTP Router
+  r := mux.NewRouter()
+
+  // When an HTTP GET request is received on the path /health, delegates to the function "HealthCheckHandler()"
+  r.HandleFunc("/health", HealthCheckHandler).Methods("GET")
+
+  return r
+}
+
+func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+  // Print a line in the logs
+  fmt.Println("HIT: healthcheck")
+
+  // Write the string "ALIVE" into the response's body
+  io.WriteString(w, "ALIVE")
+
+  // End of the function: return HTTP 200 by default
+}
+Project Life-cycle
+The life-cycle of this project is the following:
+
+“build”: compile the source code of the application to a binary named awesome-api (the name awesome-api comes from the command go mod init github.com/<your github handle>/awesome-api) with the command go build. The first build may takes some times.
+
+“run”: Run the application in background by executing the binary awesome-api, and write logs into a file named 
